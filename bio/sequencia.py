f43c9c38afbb78c90633003ac07f54ec97cdb533
Cleuson Santos <cleusonss@gmail.com>
2025-07-12 19:53:31 +0000

    cria metodos em sequencia

--- bio/sequencia.py
+++ bio/sequencia.py
@@ -1,3 +1,5 @@
+from bio.constantes import DNA_PARA_AMINOACIDO
+from bio.constantes import DNA_STOP_CODONS
 class Sequencia:
 
     def __init__(self, sequencia):
@@ -23,3 +25,47 @@ class Sequencia:
     
     def calcular_sequencia(self):
         return len(self.sequencia)
+    
+    def complementar(self, inplace: bool = False):
+        comp_dict = str.maketrans("ATCGatcg", "TAGCtagc")
+        comp = self.sequencia.translate(comp_dict)
+        return Sequencia(comp)
+    
+    def complementar_reversa(self):
+        comp = self.complementar().sequencia[::-1]
+        return Sequencia(comp)
+
+    def transcrever(self):
+        trans_dict = str.maketrans("Tt", "Uu")
+        trans = self.sequencia.translate(trans_dict)
+        return Sequencia(trans)
+    
+    def traduzir(self, parar):
+        proteina = []
+        seq = self.sequencia
+        
+        for i in range(0, len(seq) - 2 , 3):
+            codon = seq[i:i+3]
+
+            if codon in DNA_STOP_CODONS:
+                aa = '*'
+            else:
+                aa = DNA_PARA_AMINOACIDO.get(codon, "X")
+
+            proteina.append(aa)
+
+            if parar and  aa == '*':
+                break
+
+        return "".join(proteina)
+    
+    def calcular_percentual(self):
+        total = self.sequencia.len
+        total_a = self.sequencia.upper().count("A")
+        total_t = self.sequencia.upper().count("T")
+        total_c = self.sequencia.upper().count("C")
+        total_g = self.sequencia.upper().count("G")
+
+        return [total, total_a, total_t, total_c, total_g]
+
+        
--- main.py
+++ main.py
@@ -4,6 +4,18 @@ caminho_do_arquivo = './arquivos/Flaviviridae-genomes.fasta'
 
 organismos_do_fasta = ler_fasta(caminho_do_arquivo)
 
-for organismo in organismos_do_fasta:
-    print(organismo.nome, ':', organismo.sequencia.calcular_sequencia())
+#for organismo in organismos_do_fasta:
+#    print(organismo.sequencia.traduzir(True))
 
+organismo = organismos_do_fasta[0]
+seq = organismo.sequencia
+
+prot = seq.traduzir(False)
+proteinas = prot.split('*');
+totals = seq.calcular_percentual
+
+# for proteina in proteinas:
+#     print(proteina)
+#     print("")
+
+print(totals)
