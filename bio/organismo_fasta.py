ac649b7b6ce081a402361f667ae12bb9778433e4
thaisab89 <thais.bertolino8@gmail.com>
2025-07-12 11:59:24 -0300

    Projeto

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -2,3 +2,7 @@ class OrganismoFasta:
     """
     Criar
     """
+    def __init__(self, id, nome, sequencia):
+        self.id = id
+        self.nome = nome
+        self.sequencia = sequencia
---
+++ main.py
@@ -0,0 +1,10 @@
+from bio.ler_fasta import ler_fasta
+
+caminho_do_arquivo = './arquivos/Flaviviridae-genomes.fasta'
+
+organismos_do_fasta = ler_fasta(caminho_do_arquivo)
+
+print('Os organismos são:')
+for organismo in organismos_do_fasta:
+    print(organismo.id)
+
